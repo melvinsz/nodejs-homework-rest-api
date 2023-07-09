b76b9708a735fa99628df89ea9bc2939766c8ce3
Stanislav <Stas.zaliiski@gmail.com>
2023-07-09 16:47:49 +0200

    test

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "test message" });
+  res.json({ message: "test" });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "test message" });
+  res.json({ message: "test" });
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "test message" });
+  res.json({ message: "test" });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "test message" });
+  res.json({ message: "test" });
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "test message" });
+  res.json({ message: "test" });
 });
 
 module.exports = router;
