a5250cc0bbc34b1c46c143f39ae331b6e95eda63
Stanislav <Stas.zaliiski@gmail.com>
2023-07-15 14:50:32 +0200

    main

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST = "mongodb+srv://Admin:Admin123@cluster0.qpleypz.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
