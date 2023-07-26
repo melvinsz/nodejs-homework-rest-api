6f2088454dbddd581a0fc06eb6e291bbebcc9750
Stanislav <Stas.zaliiski@gmail.com>
2023-07-26 21:38:10 +0200

    main

--- models/user.js
+++ models/user.js
@@ -31,6 +31,7 @@ const userSchema = new Schema(
       default: "starter",
     },
     token: { type: String, default: "" },
+    avatarURL: { type: String, required: true },
   },
   { versionKey: false, timestamps: true }
 );
